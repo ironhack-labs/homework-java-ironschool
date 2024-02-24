e2c1c3dec86ffdb352ef95cf9d9c1517ec7e754f
Sergi Vaqué <sergi.vaque@allianz.com>
2024-02-24 10:32:09 +0100

    Basic Teacher version

---
+++ src/main/java/model/Teacher.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.UUID;
+
+public class Teacher {
+    private final String teacherId;
+    private String name;
+    private double salary;
+
+    public Teacher(String name, double salary) {
+        this.teacherId = UUID.randomUUID().toString();
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
