3a00d3dcaad8d69aa16ec43f03a468e968b8260c
Xavi Soria <xsbertran@gmail.com>
2024-02-24 15:14:24 +0100

    save commit after merge to this branch

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
@@ -5,7 +7,7 @@ public class Application {
     public static void main(String[] args) {
 
         createSchoolName();
-        numberOfTeachers();
+        teacherDetails();
 
 
     }
@@ -29,14 +31,45 @@ public class Application {
                 scanner.next();
             }
             numberofTeachers = scanner.nextInt();
+
+            if (numberofTeachers <= 0) {
+                System.err.println("The number of teachers can't be 0 or negative");
+            } else if (numberofTeachers == 1) {
+                System.out.println("In your school there is only " + numberofTeachers + " teacher");
+            } else {
+                System.out.println("In your school there are " + numberofTeachers + " teachers");
+            }
+
         } while (numberofTeachers <= 0);
 
 
-        System.out.println("In your school there are: " + numberofTeachers + " teachers");
         return numberofTeachers;
 
-
         }
+
+        private static Map<String,Teacher> teacherDetails() {
+            Map<String,Teacher> teachersMap = new HashMap<>();
+            int numberOfTeachers = numberOfTeachers();
+            scanner.nextLine();
+            for (int i = 0; i < numberOfTeachers; i++) {
+                System.out.println("Enter details for Teacher " + (i + 1) + ":");
+
+                System.out.println("Name: ");
+                String name = scanner.nextLine();
+
+                System.out.println("Salary: ");
+                double salary = Double.parseDouble(scanner.nextLine());
+                Teacher teacher = new Teacher(name, salary);
+                teachersMap.put(teacher.getTeacherId(), teacher );
+            }
+
+            for (Map.Entry<String, Teacher> entry : teachersMap.entrySet()) {
+                System.out.println("Teacher ID: " + entry.getKey() + ", Name: " + entry.getValue().getName() + ", Salary: " + entry.getValue().getSalary());
+            }
+
+            return teachersMap;
+
+    }
 
 }
 
