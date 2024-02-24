f04c24f91c54e6a2b6f607f9610e7019f5b408ac
jtiradohernandez <javiertirhe@gmail.com>
2024-02-24 13:30:24 +0100

    Change from arrays to maps in Command class

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,41 +1,82 @@
-public class Commands {
-    private Course[] courseList;
-    private Student[] studentList;
-    private Teacher[] teacherList;
+import java.util.HashMap;
+import java.util.Map;
 
-    public Commands(Course[] courseList, Student[] studentList, Teacher[] teacherList) {
+public class Commands {
+    private Map<String, Course> courseList = new HashMap<>();
+    private Map<String, Student> studentList = new HashMap<>();
+    private Map<String, Teacher> teacherList = new HashMap<>();
+
+
+    public Commands(Map<String, Course> courseList, Map<String, Student> studentList, Map<String, Teacher> teacherList) {
         setCourseList(courseList);
         setStudentList(studentList);
         setTeacherList(teacherList);
     }
 
-    public Course[] getCourseList() {
+    public Map<String, Course> getCourseList() {
         return courseList;
     }
 
-    public void setCourseList(Course[] courseList) {
+    public void setCourseList(Map<String, Course> courseList) {
         this.courseList = courseList;
     }
 
-    public Student[] getStudentList() {
+    public Map<String, Student> getStudentList() {
         return studentList;
     }
 
-    public void setStudentList(Student[] studentList) {
+    public void setStudentList(Map<String, Student> studentList) {
         this.studentList = studentList;
     }
 
-    public Teacher[] getTeacherList() {
+    public Map<String, Teacher> getTeacherList() {
         return teacherList;
     }
 
-    public void setTeacherList(Teacher[] teacherList) {
+    public void setTeacherList(Map<String, Teacher> teacherList) {
         this.teacherList = teacherList;
     }
 
-    public void enroll(String studentID, String courseID){ // This command will help enroll the student specified in the corresponding course. While also updating the money_earned of that course based on its price
-        int length = getCourseList().length;
+    public void commandSelector(CommandEnum commandAction){
+        String studentID = "asdfasdf";
+        String courseID = "asdfasdf";
+        String teacherID = "asdfasdf";
+        switch (commandAction){
+        case ENROLL:
 
+            enroll(studentID,courseID);
+            break;
+        case ASSIGN:
+            assign(teacherID,courseID);
+            break;
+        case SHOW_COURSES:
+            ShowCourses();
+            break;
+        case LOOKUP_COURSE:
+            LookupCourse(courseID);
+            break;
+        case SHOW_STUDENTS:
+            ShowStudents();
+            break;
+        case LOOKUP_STUDENT:
+            LookupStudent(studentID);
+            break;
+        case SHOW_TEACHERS:
+            ShowTeachers();
+            break;
+        case LOOKUP_TEACHER:
+            LookupTeacher(teacherID);
+            break;
+        case SHOW_PROFIT:
+            ShowProfit();
+            break;
+        default:
+            System.err.println("The command selected is not available");
+            break;
+        }
+    }
+
+    public void enroll(String studentID, String courseID){ // This command will help enroll the student specified in the corresponding course. While also updating the money_earned of that course based on its price
     }
 
     public void assign(String teacherID,String courseID){   //This command will help assign the teacher specified to the corresponding course
