eb488f7e561f8a47d2b20e6f32f1fe965019c800
Estefania Amundaray <estefania.amundaray@emburse.com>
2024-02-24 13:01:56 +0100

    add constructor to class course

--- Ironhack-school/src/main/java/ironhack/com/Course.java
+++ Ironhack-school/src/main/java/ironhack/com/Course.java
@@ -12,7 +12,16 @@ public class Course {
     private double money_earned;
     private Teacher teacher;
 
+    public void course(String name, double price){
+        super(name);
+        setPrice(price);
+    }
+    public void course(String name, double price,Teacher teacher){
+        super(name);
+        setPrice(price);
+        setTeacher(teacher);
+    }
     public void assign(String teacherId) {
-        // logica
+        setTeacher();
     }
 }
