1bc2e6575749fdc617e911cb210589a6c5d4d0d9
katalinjordansgmail <katalinjordans@gmail.com>
2024-02-24 14:25:47 +0100

    Addition of logic in lookup methods

--- Ironhack-school/src/main/java/ironhack/com/School.java
+++ Ironhack-school/src/main/java/ironhack/com/School.java
@@ -1,6 +1,11 @@
 package ironhack.com;
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
+import java.util.Objects;
 
+@Getter
+@Setter
 public class School {
     private List<Student> studentList;
     private List<Teacher> teacherList;
@@ -11,14 +16,33 @@ public class School {
     }
 
     private Student lookupStudent(String studentId) {
+        for(Student student : studentList) {
+            if (Objects.equals(student.getId(), studentId)) {
+                return student;
+            }
+        }
         return null;
     }
 
+//    private Object lookupObject(String id, List<Object> objectList) {
+//        for(Object object : objectList) {
+//            if (Objects.equals(object.getId(), id)) {
+//                return object;
+//            }
+//        }
+//        return null;
+//    }
+
     private void showCourses() {
-        // codigo
+
     }
 
     private Course lookupCourse(String courseId) {
+        for(Course course : courseList) {
+            if (Objects.equals(course.getId(), courseId)) {
+                return course;
+            }
+        }
         return null;
     }
 
@@ -27,6 +51,11 @@ public class School {
     }
 
     private Teacher lookupTeacher(String teacherId) {
+        for(Teacher teacher : teacherList) {
+            if (Objects.equals(teacher.getId(), teacherId)) {
+                return teacher;
+            }
+        }
         return null;
     }
 
