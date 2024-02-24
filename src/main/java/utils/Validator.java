0907cf628dee7aecedb884469bed30db188db7c9
Sergi Vaqué <sergi.vaque@allianz.com>
2024-02-24 21:23:57 +0100

    Validator class

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -16,9 +16,9 @@ public class Validator {
         return patternMatches(email, "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
     }
 
-    public static boolean patternMatches(String emailAddress, String regexPattern) {
+    public static boolean patternMatches(String inputString, String regexPattern) {
         return Pattern.compile(regexPattern)
-                .matcher(emailAddress)
+                .matcher(inputString)
                 .matches();
     }
 }
