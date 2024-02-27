933d6c2a296786f16c2b7bbb9a36b94902b53f20
JuliaORS <julia.olle.roset@gmail.com>
2024-02-27 18:12:27 +0100

    update command utils

--- src/main/java/org/CommandUtils.java
+++ src/main/java/org/CommandUtils.java
@@ -7,11 +7,11 @@ public class CommandUtils {
     public static void showAll(HashMap<String, ?> map){
         map.forEach((id, obj) -> {
             if (obj instanceof Teacher) {
-                ((Teacher)obj).getInfo();
+                System.out.println(((Teacher)obj).getInfo());
             } else if (obj instanceof Course) {
-                ((Course)obj).getInfo();
+                System.out.println(((Course)obj).getInfo());
             } else if (obj instanceof Student) {
-                ((Student)obj).getInfo();
+                System.out.println(((Student)obj).getInfo());
             }
         });
     }
@@ -19,11 +19,11 @@ public class CommandUtils {
     public static void lookUp(HashMap<String, ?> map, String targetId){
         map.forEach((id, obj) -> {
             if (obj instanceof Teacher && id.equals(targetId)) {
-                ((Teacher)obj).getInfo();
+                System.out.println(((Teacher)obj).getInfo());
             } else if (obj instanceof Course && id.equals(targetId)) {
-                ((Course)obj).getInfo();
+                System.out.println(((Course)obj).getInfo());
             } else if (obj instanceof Student && id.equals(targetId)) {
-                ((Student)obj).getInfo();
+                System.out.println(((Student)obj).getInfo());
             }
         });
     }
