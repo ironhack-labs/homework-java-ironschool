7c12d2dd725905dc7ce90297f1be1a7c6532a8d8
Arian <arian.collaso.rodriguez@gmail.com>
2024-02-27 15:54:19 +0100

    Refactor Menu test

--- src/test/java/MenuTest.java
+++ src/test/java/MenuTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -29,38 +28,30 @@ class MenuTest {
 
     @ParameterizedTest
     @DisplayName("ShouldReturn NoSuchElementException")
-    @ValueSource(strings = {"kdf\n", "s\n", "d\n", "dh\n"})
+    @ValueSource(strings = {"pex\n", "dos\n", "NotValid\n", "!!!!!!!!!!\n"})
     public void testShowPrincipalMenuAndRetrieveOption_InvalidInput(String value){
         ByteArrayInputStream input = new ByteArrayInputStream(value.getBytes());
         Scanner sc = new Scanner(input);
         assertThrows(NoSuchElementException.class, () -> Menu.showPrincipalMenuAndRetrieveOption(sc));
-        System.out.println("Error occurred for input value: " + value + " - ");
     }
 
 
-    @ParameterizedTest
-    @DisplayName("Should return Invalid Option on first input")
-    @ValueSource(strings = {"5\n1\n", "12\n1\n", "18\n1\n", "19\n1\n"})
-    public void testInvalidOptionMessageParameterizedTest(String value) {
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        InputStream inputStream = new ByteArrayInputStream(value.getBytes()); // lanzamos primero el malo y lo capturamos y luego liberamos lanzando el bueno.
-        Scanner scanner = new Scanner(inputStream);
-        System.setIn(inputStream);
-        Menu.showPrincipalMenuAndRetrieveOption(scanner);
-        // Capture the output in a variable before resetting the content of the ByteArrayOutputStream
-        String capturedOutput = outContent.toString();
-        // Reset the content of the ByteArrayOutputStream after the relevant method call
-        outContent.reset();
-        // Find the index of the specific message in the captured output
-        String errorMessage = "Invalid option. Please enter either 1 or 2";
-        int startIndex = capturedOutput.indexOf(errorMessage);
-        int endIndex = startIndex + errorMessage.length();
-        // Extract the specific message from the captured output
-        String expectedOutput = capturedOutput.substring(startIndex, endIndex);
-        // Verify if the captured output contains the expected message
-        assertEquals(errorMessage, expectedOutput.trim());
-    }
+@ParameterizedTest
+@DisplayName("Should return Invalid Option on first input")
+@ValueSource(strings = {"5\n1\n", "1928\n1\n", "72863\n1\n", "9999999\n1\n"})
+public void testInvalidOptionMessageParameterizedTest(String value) {
+    ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+    System.setErr(new PrintStream(errorStream));
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(value.getBytes());
+    Scanner sc = new Scanner(inputStream);
+    Menu.showPrincipalMenuAndRetrieveOption(sc);
+    String errorOutput = errorStream.toString();
+    assertTrue(errorOutput.contains("Invalid option. Please enter either 1 or 2"));
+    System.setErr(System.err);
+}
+
+
+
 
 
 //    @Test
