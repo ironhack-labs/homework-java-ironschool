7f116dde2cdcdba05c5b4e5ecf586a04d43a60f3
evagapi <eva.gapi@gmail.com>
2024-02-27 17:38:30 +0100

    feat: add School class (#7)

---
+++ src/main/java/model/School.java
@@ -0,0 +1,63 @@
+package model;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class School {
+    @Setter(AccessLevel.NONE)
+    private final String id;
+    private String name;
+    private Map<String, Teacher> teachers = new HashMap<>();
+    private Map<String, Course> courses = new HashMap<>();
+    private Map<String, Student> students = new HashMap<>();
+
+    public School(String name) {
+        id = UUID.randomUUID().toString();
+        this.name = name;
+    }
+
+    public void addCourse(Course course) {
+        courses.put(course.getCourseId(), course);
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.put(teacher.getTeacherId(), teacher);
+    }
+
+    public void addStudent(Student student) {
+        students.put(student.getStudentId(), student);
+    }
+
+    public double getTotalProfit() {
+        return getTotalEarned() - computeSalaries();
+    }
+
+    private double computeSalaries() {
+        double totalSpent = 0;
+        for (Map.Entry<String, Teacher> entry : teachers.entrySet()) {
+            Teacher teacher = entry.getValue();
+            double spent = teacher.getSalary();
+            totalSpent += spent;
+        }
+        return totalSpent;
+    }
+
+    private double getTotalEarned() {
+        double totalEarned = 0;
+        for (Map.Entry<String, Course> entry : courses.entrySet()) {
+            Course course = entry.getValue();
+            double earned = course.getMoney_earned();
+            totalEarned += earned;
+        }
+        return totalEarned;
+    }
+}
---
+++ src/test/java/model/SchoolTest.java
@@ -0,0 +1,45 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class SchoolTest {
+    @Test
+    @DisplayName("two different schools should have two different ids")
+    void School_multipleInstancesOfSchoolDifferentIds() {
+        School maristes = new School("Maristes");
+        School balmes = new School("Jaume Balmes");
+        assertNotEquals(maristes.getId(), balmes.getId());
+    }
+
+    @Test
+    @DisplayName("subtract teachers salaries from total earned from courses")
+    void School_subtractSalariesFromTotalEarnedFromCourses() {
+        School balmes = new School("Jaume Balmes");
+
+        Teacher anne = new Teacher("Anne", 1200.00);
+        Teacher john = new Teacher("John", 1200.00);
+        Course englishCourseA1 = new Course("English A1", 300.00);
+        Course englishCourseA2 = new Course("English A2", 300.00);
+        Course englishCourseB1 = new Course("English B1", 300.00);
+        Course englishCourseB2 = new Course("English B2", 300.00);
+
+        balmes.addTeacher(anne);
+        balmes.addTeacher(john);
+
+        englishCourseA1.setMoney_earned(1200);
+        englishCourseA2.setMoney_earned(900);
+        englishCourseB1.setMoney_earned(1500);
+        englishCourseB2.setMoney_earned(1200);
+
+        balmes.addCourse(englishCourseA1);
+        balmes.addCourse(englishCourseA2);
+        balmes.addCourse(englishCourseB1);
+        balmes.addCourse(englishCourseB2);
+
+        assertEquals(balmes.getTotalProfit(), 2400.00);
+    }
+}
