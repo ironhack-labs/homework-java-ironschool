33e71348c1e2716cdf5ddb1e6691bf69c70c79f7
“wardin231” <didiazhernandez@gmail.com>
2024-02-29 19:01:28 +0100

    commands 3 to 8 advance

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -50,27 +51,35 @@ public class Commands {
         case ASSIGN:
             assign(teacherID,courseID);
             break;
+
         case SHOW_COURSES:
             ShowCourses();
             break;
+
         case LOOKUP_COURSE:
             LookupCourse(courseID);
             break;
+
         case SHOW_STUDENTS:
             ShowStudents();
             break;
+
         case LOOKUP_STUDENT:
             LookupStudent(studentID);
             break;
+
         case SHOW_TEACHERS:
             ShowTeachers();
             break;
+
         case LOOKUP_TEACHER:
             LookupTeacher(teacherID);
             break;
+
         case SHOW_PROFIT:
             ShowProfit();
             break;
+
         default:
             System.err.println("The command selected is not available");
             break;
@@ -94,27 +103,47 @@ public class Commands {
     }
 
     public void ShowCourses(){    //This command will display a list of all courses
-
+        for (Course course : courseList.values()){
+            System.out.println(course.getName());
+        };
     }
 
     public void LookupCourse(String courseID){ // This command will display the full details of the specified course
+        Course x = courseList.get(courseID);
 
+        System.out.println(x.getName());
+        System.out.println(x.getPrice());
+        System.out.println(x.getTeacher());
+        System.out.println(x.getClass());
     }
 
     public void ShowStudents(){ //This command will display a list of all students
-
+        for (Student student : studentList.values()){
+            System.out.println(student.getName());
+        }
     }
 
-    public void LookupStudent(String studentID){ //This command will display the full details of the specified student
+    public void LookupStudent(Student studentID){ //This command will display the full details of the specified student
+        Student student = studentList.get(studentID);
 
+        System.out.println(student.getName());
+        System.out.println(student.getAddress());
+        System.out.println(student.getCourse());
+        System.out.println(student.getEmail());
     }
 
     public void ShowTeachers(){ //This command will display a list of all teachers
-
+        for (Teacher teacher : teacherList.values()){
+            System.out.println(teacher.getName());
+        }
     }
 
     public void LookupTeacher(String teacherID){ // This command will display the full details of the specified teacher
+        Teacher teacher = teacherList.get(teacherID);
 
+        System.out.println(teacher.getName());
+        System.out.println(teacher.getSalary());
+        System.out.println(teacher.getClass());
     }
 
     public void ShowProfit(){ //This command will calculate (The total money earned from all courses) - (The sum of all the teachers' salaries) and return the result
