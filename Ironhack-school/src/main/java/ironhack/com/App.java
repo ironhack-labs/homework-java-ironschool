2f2f7e3365ecb2e7427f0bea8da8f8790363f44a
Estefania Amundaray <estefania.amundaray@emburse.com>
2024-02-29 22:05:56 +0100

    menu creation

--- Ironhack-school/src/main/java/ironhack/com/App.java
+++ Ironhack-school/src/main/java/ironhack/com/App.java
@@ -1,34 +1,63 @@
 package ironhack.com;
 
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static ironhack.com.ReadCsvInfo.readObjectInfo;
 import static ironhack.com.ReadCsvInfo.readSchoolInfo;
+import static ironhack.com.Utils.validate_number_of;
 
 public class App
 {
-    public static void main( String[] args ) throws FileNotFoundException {
-        String[] school_info = readSchoolInfo();
-        School school = new School(school_info[0]);
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
 
-        List<Teacher> teachers = readObjectInfo("src/files/Teachers.csv",1,RoleType.TEACHER);
-        List<Student> students = readObjectInfo("src/files/Students.csv",2,RoleType.STUDENT);
-        List<Course> courses = readObjectInfo("src/files/Courses.csv",2,RoleType.COURSE);
+        boolean is_finish= false;
+        do{
 
-        school.setListToStudentMap(students);
-        school.setListToTeacherMap(teachers);
-        school.setListToCourseMap(courses);
 
-        System.out.println("All Courses");
-        school.showCourses();
+            System.out.println("*******School Management System******");
 
-        System.out.println("All Teachers");
-        school.showTeachers();
+            System.out.println( "Let's create an School");
+            System.out.println( "Enter a Name:");
+            scanner.nextLine();
 
-        System.out.println("All Students");
-        school.showStudents();
+            System.out.println("how many teachers should be created?");
+            int teacher_number = validate_number_of(scanner);
+            System.out.println(teacher_number);
+            create_teachers(scanner,teacher_number);
+
+            System.out.println("how many courses should be created?");
+            int courses_number = validate_number_of(scanner);
+            System.out.println(courses_number);
+
+            System.out.println("how many students should be created?");
+            int students_number = validate_number_of(scanner);
+            //TODO validate more than 0
+            System.out.println(students_number);
+
+
+            //more than 0 and
+            is_finish=true;
+
+        }while(!is_finish);
     }
+
+    public static void create_teachers(Scanner scanner, int teacher_number){
+
+        List<Teacher> teachers_list= new ArrayList<>();
+        for (int i=1; i<teacher_number ; i++){
+            System.out.println("Let's create teacher {i}");
+            System.out.println("Enter Name:");
+            String teacher_name = scanner.nextLine();
+
+            System.out.println("Enter Salary (Ex 34000): ");
+            int teacher_salary=validate_number_of(scanner);
+            //Validate Range of Salaries
+            teachers_list.add(new Teacher(teacher_name,teacher_salary));
+            //school.setListToTeacherMap(teachers);
+        }
+
+    }
+
 }
