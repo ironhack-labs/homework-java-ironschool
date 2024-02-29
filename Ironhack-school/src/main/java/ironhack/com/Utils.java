3a39d79ee5ab4902cfa32779a87c7fb2cd132642
Estefania Amundaray <estefania.amundaray@emburse.com>
2024-02-29 22:05:37 +0100

    validate_number_of method

--- Ironhack-school/src/main/java/ironhack/com/Utils.java
+++ Ironhack-school/src/main/java/ironhack/com/Utils.java
@@ -1,4 +1,5 @@
 package ironhack.com;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Utils {
@@ -6,5 +7,21 @@ public class Utils {
     public static String generateSerialId () {
 
         return UUID.randomUUID().toString();
+    }
+
+    public static int validate_number_of(Scanner scanner){
+        boolean is_finish= false;
+
+        do{
+            String number_string=scanner.nextLine();
+            try{
+                int number= Integer.parseInt(number_string);
+                return number;
+            } catch (NumberFormatException e){
+                System.out.println("Enter a valid number");
+            }
+            is_finish=true;
+        }while(is_finish);
+        return 0;
     }
 }
