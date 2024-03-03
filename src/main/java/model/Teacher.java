6ef3714f43fbb8cbdb45befbf9afe3517a732af5
Jose Vazquez <vazquezjoseman13@gmail.com>
2024-03-03 13:49:38 +0100

    Teacher class update for Test

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,27 +1,29 @@
 package model;
-
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-
 import java.util.UUID;
 
 @Getter
 @Setter
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Teacher {
 
     @Setter(AccessLevel.NONE)
+    @EqualsAndHashCode.Include
     private final String teacherId;
+
+    @EqualsAndHashCode.Include
     private String name;
+
+    @EqualsAndHashCode.Include
     private double salary;
 
     public Teacher(String name, double salary) {
-        teacherId = UUID.randomUUID().toString();
+        this.teacherId = UUID.randomUUID().toString();
         this.name = name;
         this.salary = salary;
     }
-
 }
