8c87a8b89c3af9dc96b7ef870aac386af5efbbec
Jose Vazquez <vazquezjoseman13@gmail.com>
2024-03-03 14:00:26 +0100

    Update Teacher class test annotations

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -4,6 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -25,5 +26,18 @@ public class Teacher {
         this.teacherId = UUID.randomUUID().toString();
         this.name = name;
         this.salary = salary;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Teacher teacher = (Teacher) o;
+        return Double.compare(teacher.salary, salary) == 0 &&
+                Objects.equals(name, teacher.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, salary);
     }
 }
