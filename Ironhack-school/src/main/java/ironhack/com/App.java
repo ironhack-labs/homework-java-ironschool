1e4748cb8c9927402aacca44afa06686fb8d80c4
Estefania Amundaray <estefania.amundaray@emburse.com>
2024-03-12 21:53:18 +0100

    commands creation

--- Ironhack-school/src/main/java/ironhack/com/App.java
+++ Ironhack-school/src/main/java/ironhack/com/App.java
@@ -33,6 +33,8 @@ public class App {
             System.out.println(students_number);
             List<Student> students =create_students(scanner, students_number);
 
+            create_school(name,teachers,courses,students);
+            show_commands_list(scanner);
 
             is_finish = true;
 
@@ -94,5 +96,70 @@ public class App {
 
     }
 
+    public static void create_school(String name, List<Teacher> teachers, List<Course> courses, List<Student> students){
+        School school = new School(name);
+        school.setListToStudentMap(students);
+        school.setListToTeacherMap(teachers);
+        school.setListToCourseMap(courses);
+    }
+
+    public static void show_commands_list(Scanner scanner, School school){
+        boolean is_finish = false;
+        do {
+            System.out.println("*******School Management System******");
+            System.out.println("Choose a Command From the List");
+            System.out.println("1-ENROLL [STUDENT_ID] [COURSE_ID]");
+            System.out.println("2-ASSIGN [TEACHER_ID] [COURSE_ID]");
+            System.out.println("3-SHOW COURSES");
+            System.out.println("4-LOOKUP COURSE [COURSE_ID]");
+            System.out.println("5-SHOW STUDENTS");
+            System.out.println("6-LOOKUP STUDENT [STUDENT_ID]");
+            System.out.println("7-SHOW TEACHERS");
+            System.out.println("8-LOOKUP TEACHER [TEACHER_ID]");
+            System.out.println("9-SHOW PROFIT");
+            System.out.println("h-help");
+            String value=scanner.nextLine();
+            String result;
+            switch (value) {
+                case 1:
+                    System.out.println("Enter a [STUDENT_ID]");
+                    int student_id=scanner.nextLine();
+                    school.enroll();
+                    break;
+                case 2:
+                    result = "Two";
+                    break;
+                case 3:
+                    school.showCourses();
+                    break;
+                case 4:
+                    result = "Four";
+                    break;
+                case 5:
+                    school.showStudents();
+                    break;
+                case 6:
+                    result = "Six";
+                    break;
+                case 7:
+                    school.showTeachers();
+                    break;
+                case 8:
+                    result = "Eight";
+                    break;
+                case 9:
+                    break;
+                case 'h':
+                    result = "Letter h";
+                    break;
+                default:
+                    result = "Invalid input";
+            }
+
+
+            is_finish = true;
+
+        } while (!is_finish);
+    }
 
 }
